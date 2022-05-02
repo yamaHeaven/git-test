38d6959d9a2351ac3fa2a8e7877721e0729cd6f1
Layne <Layne@atguigu.com>
2022-05-02 10:11:04 +0800

    "第一次使用idea集合git提交的代码"

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author 黄发林
+ * @version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
