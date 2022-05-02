db9e4577e6c1e92e381d8c6b5cdf8efe826d166e
Layne <Layne@atguigu.com>
2022-05-02 10:15:19 +0800

    "第三次使用idea集合git提交的代码 修改为git3 "

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
         System.out.println("hello git2 第一次修改!");
+        System.out.println("hello git3 第二次修改!");
     }
 }
